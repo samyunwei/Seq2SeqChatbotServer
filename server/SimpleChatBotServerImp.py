16f23fe49f45670ef294620688781d134ab9dfb9
Samyunwei <259375702@qq.com>
2020-06-26 01:13:53 +0800

    first commit

---
+++ server/SimpleChatBotServerImp.py
@@ -0,0 +1,14 @@
+from server.pb import SimpleChatbot_pb2_grpc
+from server.pb.SimpleChatbot_pb2 import ChatReply
+
+
+class SimpleChatBotServicerImp(SimpleChatbot_pb2_grpc.SimpleChatBotServerServicer):
+    def __init__(self, processor):
+        self.processor = processor
+
+    def Chat(self, request, context):
+        print("Seq2SeqChatBot:New Req  ids %s" % request.ids)
+        rep = ChatReply()
+        rep.ids = request.ids
+        rep.data = self.processor.process(request.data)
+        return rep
---
+++ server/SimpleChatProcessor.py
@@ -0,0 +1,10 @@
+import os
+
+
+class SimpleChatProcessor(object):
+    def __init__(self):
+        pass
+
+    def process(self, input):
+        output_words = "Hello World!"
+        return output_words
